cf69b7583957534e216ea66e2597e67492ac12bd
hslape <awinter71417@gmail.com>
2024-04-14 20:57:20 -0500

    Add new products to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[ 
+    {
+        "name": "Dice Set",
+        "category": "Dice",
+        "price": 10.00,
+        "description": "A set of 7 polyhedral dice for tabletop roleplaying games.",
+        "image_url": "http://source.unsplash.com/200x200/?dice"
+    },
+    {
+        "name": "Player's Handbook",
+        "category": "Books",
+        "price": 50.00,
+        "description": "The essential reference for every Dungeons & Dragons player.",
+        "image_url": "http://source.unsplash.com/200x200/?book"
+    }
+]
